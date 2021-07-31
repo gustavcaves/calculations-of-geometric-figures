a78662d00b1e8b6b56370ae5f415041b95ad6dc1
Gustavo Adolfo Cuevas García <gustavcaves@MacBook-Air-de-Gustav-Caves.local>
2021-07-31 19:13:57 +0200

    tailwind y reto

--- figuras.js
+++ figuras.js
@@ -93,20 +93,100 @@ function areaDelCirculo(radio) {
     return (radio * radio) * Math.PI
 }
 
+
+// En este ejercicio debes crear una función para calcular la altura de un triángulo isósceles.
+
+// La función debe recibir, como parámetros, la longitud de los 3 lados del triángulo.
+// La función debe validar que la longitud de los 3 lados del triángulo corresponden a un triángulo isósceles.
+// La función debe retornar la altura del triángulo.
+// Pista: la función Math.sqrt de JavaScript puede ayudarte a calcular raíces cuadradas.
+
+function alturaDeTrianguloIsoseles(lado1, base) {
+    const altura = Math.sqrt((lado1**2)-((base/2)**2));
+    return altura
+}
+
 // console.groupEnd()
 
 // Aquí interactuamos con el HTML
 
+// Cuadrados
+
 function calcularPerimetroCuadrado(params) {
     const input = document.getElementById("InputCuadrado");
-    const value = input.value;
+    const value = parseInt(input.value);
     const perimeto = perimetroCuadrado(value);
     alert(perimeto);
 }
 
 function calcularAreaCuadrado(params) {
     const input = document.getElementById("InputCuadrado");
-    const value = input.value;
+    const value = parseInt(input.value);
     const area = areaDelCuadrado(value);
     alert(area);
+}
+
+// Triangulos
+
+function calcularPerimetroTriangulo(params) {
+    const input1 = document.getElementById("InputLadoTriangulo1");
+    const value1 = parseInt(input1.value);
+    const input2 = document.getElementById("InputLadoTriangulo2");
+    const value2 = parseInt(input2.value);
+    const input3 = document.getElementById("InputBaseTriangulo");
+    const value3 = parseInt(input3.value);
+    const perimetro = perimetroDelTriangulo(value1,value2,value3)
+    alert(perimetro);
+}
+
+function calcularAreaTriangulo(params) {
+    const input1 = document.getElementById("InputBaseTriangulo");
+    const value1 = parseInt(input1.value);
+    const input2 = document.getElementById("InputAlturaTriangulo");
+    const value2 = parseInt(input2.value);
+    const area = areaDelTriangulo(value1,value2)
+    alert(area);
+}
+
+function calcularAlturaTriangulo(params) {
+    const input1 = document.getElementById("InputLadoTriangulo1");
+    const value1 = parseInt(input1.value);
+    const input2 = document.getElementById("InputLadoTriangulo2");
+    const value2 = parseInt(input2.value);
+    const input3 = document.getElementById("InputBaseTriangulo");
+    const value3 = parseInt(input3.value);
+
+    if (value1 == value2) {
+        alert("El triangulo es isoseles")
+        const altura = alturaDeTrianguloIsoseles(value1,value3)
+        alert(altura);    
+    } else {
+        alert("El triangulo no es isoseles debe intentar nuevamente.")
+    }
+
+    
+}
+
+// Circunferencias
+
+
+function calcularDiametroCirculo(params) {
+    const input = document.getElementById("InputRadioCirculo")
+    const value = parseInt(input.value)
+    const diametro = diametroCirculo(value)
+    alert(diametro)
+}
+
+function calcularPerimetroCirculo(params) {
+    const input = document.getElementById("InputRadioCirculo")
+    const value = parseInt(input.value)
+    const perimeto = perimetroCirculo(value)
+    alert(perimeto)
+}
+
+function calcularAreaCirculo(params) {
+    const input = document.getElementById("InputRadioCirculo")
+    const value = parseInt(input.value)
+    const area = areaDelCirculo(value)
+    alert(area)
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [],
+  darkMode: media, // or 'media' or 'class' or 'false'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
